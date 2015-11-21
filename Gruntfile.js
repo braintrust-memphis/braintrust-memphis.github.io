2a8b4a3df9f2fb17337c710b78953f90afdd9169
Geoff Davis <gdavis@archermalmo.com>
2015-11-21 16:41:37 -0600

    Update

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		watch: {
+			default: {
+				options: {
+					livreload: 29929
+				},
+				files: ["_site/*.html","_site/css/*.css","_site/js/*.js"]
+			},
+			dev: {
+				files: ['js/es6/*.js'],
+				tasks: 'babel:dev'
+			}
+		},
+		copy: {
+			main: {
+				files: {
+					'js/lib/jquery.min.js' : 'node_modules/jquery/dist/jquery.min.js'
+				}
+			}
+		},
+		babel: {
+			dev: {
+				options:{
+					sourceMap: 'linked',
+					presets: ['es2015']
+				},
+				files: [{
+					expand: true,
+					cwd: './js/es6',
+					src: ['*.js'],
+					dest: './js/',
+					ext: '.js'
+				}]
+			},
+			build: {
+				options:{
+					sourceMap: false,
+					presets: ['es2015']
+				},
+				files: [{
+					expand: true,
+					cwd: './js/',
+					src: ['**/*.js'],
+					dest: './_site/js/',
+					ext: '.js'
+				}]
+			}
+		},
+		uglify: {
+			options: {
+				mangle: true,
+				compress: true,
+				sourceMap: false
+			},
+			build: {
+				files: {
+					'_site/js/init.min.js':'_site/js/init.js'
+				}
+			}
+		}
+	})
+
+	grunt.loadNpmTasks('grunt-contrib-copy')
+	grunt.loadNpmTasks('grunt-contrib-watch')
+	grunt.loadNpmTasks('grunt-babel')
+	grunt.loadNpmTasks('grunt-contrib-uglify')
+
+	grunt.registerTask("default", ['copy:main','watch:dev'])
+	grunt.registerTask('build', ['babel:build','uglify:build'])
+}
---
+++ js/es6/init.js
@@ -0,0 +1,10 @@
+// INIT
+
+$('a.section-link').each(function(i,el) {
+	var id = $(el).attr('data-id')
+	$(el).on('click', function(e) {
+		console.log(id)
+		window.open(`#${id}`,'_self')
+		$('body').animate({scrollTop: '+=25px'},250)
+	})
+})
