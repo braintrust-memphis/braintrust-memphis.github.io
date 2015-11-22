4878b2872f9a01b5bd933a887f33258047a1a934
Geoff Davis <gdavis@archermalmo.com>
2015-11-22 13:45:51 -0600

    presentation ready

--- src/js/init.js
+++ src/js/init.js
@@ -5,7 +5,9 @@ $('a.section-link').each(function(i,el) {
 	$(el).on('click', function(e) {
 		console.log(id)
 		window.open('#'+id,'_self')
-		$('body').animate({scrollTop: '-=40px'},0)
+			if(id !== 'contact') {
+				$('body').animate({scrollTop: '-=40px'},0)
+			}
 		if($(window).width() <= 767) {
 			$('button[data-target]').click()
 			console.log('click')
